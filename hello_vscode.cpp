d86b8246a02ceb14dfd193ae3d2a76fd8be45539
NAHDI51 <hossainnahdi8051@gmail.com>
2025-12-15 20:21:37 +0600

    Pushed from vscode-archlinux

---
+++ hello_vscode.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << "Hello from vscode. Hope to be served well.\n";
+    return 0;
+}
--- sublime_template.cpp
+++ sublime_template.cpp
@@ -1,41 +1,4 @@
-#include <algorithm>
-#include <bitset>
-#include <complex>
-#include <deque>
-#include <exception>
-#include <fstream>
-#include <functional>
-#include <iomanip>
-#include <ios>
-#include <iosfwd>
-#include <iostream>
-#include <istream>
-#include <iterator>
-#include <limits>
-#include <list>
-#include <locale>
-#include <map>
-#include <memory>
-#include <new>
-#include <numeric>
-#include <ostream>
-#include <queue>
-#include <set>
-#include <sstream>
-#include <stack>
-#include <stdexcept>
-#include <streambuf>
-#include <string>
-#include <typeinfo>
-#include <utility>
-#include <valarray>
-#include <vector>
-#include <array>
-#include <unordered_map>
-#include <unordered_set>
-#include <initializer_list>
-#include <tuple>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 
